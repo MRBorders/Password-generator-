ac03a042ca7b63ee92dfc800f716aa12b0d4c44b
matt b <mathewborders.borders4@gmail.com>
2022-03-21 21:06:55 -0400

    initial commit

---
+++ assets/Untitled-1.js
@@ -0,0 +1,38 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+function generatePassword(){
+var  uppercase= "QWERTYUIOPASDFGHJKLZXCVBNM"
+var lowercase="qwertyuiopasdfghjklzxcvbnm"
+var specialChar="!@#$%@^&*"
+var numericalValues= 1234567890
+
+var uppercaseQuestion= window.confirm ("Do you wanter uppercase letters in your generated password?")
+  if(uppercaseQuestion=== true){
+  console.log(uppercase);
+
+  }
+  
+
+  var lowercaseQuestion=window.confirm("Do you want lowercase characters in your generated password?") 
+  if (lowercaseQuestion===true){
+    console.log(lowercase);
+  }
+    
+  
+var specialCharQuestion= window.confirm ("Do you want special characters in your password?")
+  if (specialCharQuestion===true){
+    console.log(specialCharQuestion)
+  }
+  
+var numericalValuesQuestion= window.confirm ("Do you want numbers in your genereated password?")
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
